00df1b60a4ce1a9627c72312a0c3c3910ecd6b82
kekkonen86 <50250247+kekkonen86@users.noreply.github.com>
2023-05-10 12:28:03 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mysql = require('mysql');
+const etag = require('etag')
+const path = require('path');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/public', express.static('public', { 'extensions': ['css'] }));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'sakila'
+  });
+  
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      return;
+    }
+    console.log('Connected to the Sakila database');
+  });
+
+  app.get('/films', (req, res) => {
+    const filmQuery = 'SELECT * FROM film';
+    const categoryQuery = 'SELECT * FROM category';
+  
+    connection.query(filmQuery, (filmError, filmResults) => {
+      if (filmError) {
+        console.error('Error executing the film query:', filmError);
+        return;
+      }
+  
+      connection.query(categoryQuery, (categoryError, categoryResults) => {
+        if (categoryError) {
+          console.error('Error executing the category query:', categoryError);
+          return;
+        }
+  
+        res.render('films', { films: filmResults, categories: categoryResults });
+      });
+    });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
